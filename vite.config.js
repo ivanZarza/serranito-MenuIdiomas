f2eb2fabd0fffc59cb59d05b1232c9f203c2009b
Iván Zarza <ivanzarzaestevez@gmail.com>
2024-01-30 09:57:00 +0100

    build to docs folder

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import path from "path"
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    outDir: path.join(__dirname, "docs"),
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
