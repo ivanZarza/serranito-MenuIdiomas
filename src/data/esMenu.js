19b4f308a52f08342cc076e121d876b2325816c3
IvanZarza <ivanzarzaestevez@gmail.com>
2024-05-24 19:06:46 +0200

    wip

--- src/data/esMenu.js
+++ src/data/esMenu.js
@@ -12,26 +12,26 @@ export const start = [
   {
     name: "Salchichon Iberico",
     price: "7.00",
-    price2:"9.00",
+    price2:"9.50",
   },
   {
     name: "Morcilla Iberica",
     price: "7.00",
-    price2:"9.00",
-  },
-  {
-    name: "Surtido Ibericos",
-    price: "16.00",
-    price2:"",
+    price2:"9.50",
   },
   {
     name: "Queso Viejo",
-    price: "8.00",
-    price2:"13.50",
+    price: "9.00",
+    price2:"14.50",
+  },
+  {
+    name: "Surtido Ibericos",
+    price: "17.00",
+    price2:"",
   },
   {
     name: "Gambas de Huelva",
-    price: "13.50",
+    price: "13.00",
     price2:"",
   },
   {
@@ -40,7 +40,7 @@ export const start = [
     price2:"",
   },
   {
-    name: "Ensalada de Pollo",
+    name: "Ensalada Cesar",
     price: "10.00",
     price2:"",
   },
@@ -51,22 +51,27 @@ export const start = [
   },
   {
     name: "Ensaladilla",
-    price: "10.00",
-    price2:"",
-  },
-  {
-    name: "Salmorejo Cordobes",
-    price: "6.00",
+    price: "7.50",
     price2:"",
   },
   {
     name: "Patata aliñada con Melva",
-    price: "5.50",
+    price: "7.50",
     price2:"",
   },
   {
-    name: "Hojaldre de Queso de Cabra con Miel de y Cebolla crujiente",
-    price: "9.00",
+  name: "Salmorejo Cordobes",
+  price: "5.50",
+  price2:"",
+  },
+  {
+    name: "Tomate aliñado con queso de cabra y balasamico",
+    price: "8.00",
+    price2:"",
+  },
+  {
+    name: "Tomate aliñado con Melva",
+    price: "8.00",
     price2:"",
   },
 ]
@@ -74,11 +79,6 @@ export const start = [
 export const bread = [
   {
     name: "Tosta de la Casa (Jamon Iberico y Salmorejo)",
-    price: "10.00",
-    price2: "",
-  },
-  {
-    name: "Tosta de Carne Mechada y Salmorejo",
     price: "10.00",
     price2: "",
   },
@@ -92,13 +92,15 @@ export const bread = [
 export const stew = [
   {
     name: "Cola de Toro",
-    price: "12.00",
+    price: "13.00",
     price2: "",
-  },  {
+  },  
+  {
     name: "Carrillada de Cerdo",
-    price: "10.00",
+    price: "11.00",
     price2:"",
-  },  {
+  }, 
+  {
     name: "Espinacas Sevillanas con garbanzos",
     price: "9,00",
     price2:"",
@@ -110,69 +112,65 @@ export const share =[
     name: "Huevos revueltos de la Casa (Champiñon,esparrago,jamon)",
     price: "11.00",
     price2: "",
-  },  {
+  },  
+  {
     name: "Huevos revueltos con Bacalao y Patata",
     price: "11.00",
     price2:"",
-  },  {
-    name: "Huevos revueltos Ibericos",
-    price: "11.00",
-    price2:"",
-  },  {
+  },  
+  {
     name: "Croquetas de Jamon o Puchero",
     price: "9.00",
     price2:"",
-  },  {
-    name: "Coquetas de Espinacas",
+  },  
+  {
+    name: "Coquetas de temporada",
     price: "9.00",
     price2:"",
   },  {
     name: "Berenjenas fritas con Miel de Caña",
     price: "8,00",
     price2:"",
-  },  {
-    name: "Parrillada de verduras",
-    price: "10.00",
-    price2:"",
-  },  {
+  },  
+  {
     name: "Lagrimitas de Pollo",
     price: "9.00",
     price2:"",
-  },  {
-    name: "Pulpo a la Brasa",
-    price: "19.00",
+  },  
+  {
+    name: "Parrillada de verduras",
+    price: "10.00",
     price2:"",
-  },  {
+  },  
+  {
     name: "Patatas Arrieras (Patatas, Langostinos, Ali-oli)",
     price: "13.00",
     price2:"",
-  },  {
+  },  
+  {
     name: "Langostinos al Ajillo",
     price: "11.00",
     price2:"",
-  },  {
-    name: "Tomate aliñado con queso de cabra y balasamico",
-    price: "8.00",
-    price2:"",
-  },{
-    name: "Tomate con Melva",
-    price: "8.00",
+  },  
+  {
+    name: "Hojaldre de Queso de Cabra con Miel de y Cebolla crujiente",
+    price: "9.00",
     price2:"",
   },
 ]
 
 export const friet = [
   {
-    name: "Sepia",
-    price: "9.00",
+    name: "Choco frito",
+    price: "10.00",
     price2:"",
   },  {
     name: "Cazon en adobo",
-    price: "9.00",
+    price: "9.50",
     price2:"",
   },  {
     name: "Boquerones",
-    price: "9.00",
+    price: "9.50",
     price2:"",
   },  {
     name: "Bacalao",
@@ -180,7 +178,7 @@ export const friet = [
     price2:"",
   },  {
     name: "Puntillas de calamar",
-    price: "8.00",
+    price: "9.50",
     price2:"",
   },  {
     name: "Chanquetes pequeños con pimiento y huevo",
@@ -188,11 +186,11 @@ export const friet = [
     price2:"",
   },  {
     name: "Tortilla de camarones",
-    price: "8.50",
+    price: "9.50",
     price2:"",
   },  {
     name: "Fritura mixta",
-    price: "19.00",
+    price: "19.50",
     price2:"",
   },
 ]
@@ -203,24 +201,29 @@ export const sea =[
     price: "9.00",
     price2:"",
   },  {
-    name: "Sepia a la Plancha",
+    name: "Choco a la Plancha",
     price: "3.50/100gr",
     price2:"",
   },  {
     name: "Lubina a la espalda con guarnicion",
-    price: "14.00",
+    price: "15.00",
     price2:"",
   },  {
     name: "Parrillada de Pescados",
-    price: "30.00",
+    price: "31.00",
     price2:"",
   },  {
     name: "Coquinas",
-    price: "14.00",
+    price: "15.00",
     price2:"",
   },  {
     name: "Ventresca de atun",
     price: "18.00",
+    price2:"",
+  }, 
+  {
+    name: "Pulpo a la Brasa",
+    price: "19.00",
     price2:"",
   },  
 ]
@@ -228,37 +231,45 @@ export const sea =[
 export const meat = [
   {
     name: "Parrilada de carne",
-    price: "23.00",
+    price: "24.00",
     price2:"",
-  },  {
+  },   
+  {
     name: "Entrecot de Ternera",
-    price: "18.00",
+    price: "20.00",
     price2:"",
-  },  {
+  }, 
+  {
     name: "Secreto de cerdo Iberico a la Brasa",
-    price: "11.00",
+    price: "11.50",
     price2:"",
-  },  {
+  },  
+  {
     name: "Pluma de cerdo con jamon iberico",
-    price: "16.00",
+    price: "17.00",
     price2:"",
-  },  {
+  },  
+  {
     name: "Punta de solomillo de cerdo con salsa al whisky",
     price: " 10.50",
     price2:"",
-  },  {
+  },  
+  {
     name: "Punta de solomillo de cerdo con salsa al roquefort",
     price: " 10.50",
     price2:"",
-  },  {
+  },  
+  {
     name: "Churrasco de cerdo iberico a la brasa",
-    price: "10.00",
+    price: "11.00",
     price2:"",
-  },  {
+  },  
+  {
     name: "gran chuleta de vaca madurada",
     price: "50.00/kg",
     price2:"",
-  },  {
+  },  
+  {
     name: "Pechuga de pollo",
     price: "10.00",
     price2:"",
@@ -268,32 +279,33 @@ export const meat = [
 export const special = [
   {
     name: "Serranito Especial(Punta de solomillo de cerdo, queso, tortilla y jamon iberico)",
-    price: "10.00",
+    price: "10.50",
     price2:"",
-  },  {
+  },  
+  {
     name: "Serranito Especial de pollo(Pollo, queso tortilla y jamon iberico)",
-    price: "10.00",
+    price: "10.50",
     price2:"",
-  },  {
+  },  
+  {
     name: "San jacobo serrano maxi (lomo dde cerdo, jamon y queso)",
     price: "10.00",
     price2:"",
-  },  {
-    name: "San jacobo de pollo( pollo jamon y queso)",
-    price: "6.50",
-    price2:"",
-  },  {
+  },  
+  {
     name: "Hamburguesa extra de buey 200gr.,bacon y queso",
-    price: "10.00",
+    price: "11.00",
     price2:"",
-  },  {
+  }, 
+  {
     name: "Huevos rotos con jamon",
     price: "11.00",
     price2:"",
-  },  {
+  },  
+  {
     name: "Paella del dia",
-    price: "9.00",
-    price2:"",
+    price: "10.00",
+    price2:"19.50",
   },
 ]
 
@@ -302,15 +314,18 @@ export const adds = [
     name: "Pan y picos",
     price: "1.00",
     price2:"",
-  },  {
+  },  
+  {
     name: "Olivas",
     price: "2.00",
     price2:"",
-  },  {
+  },  
+  {
     name: "Salsas varias",
     price: "0.80",
     price2:"",
-  },  {
+  },  
+  {
     name: "Mahonesa o Ketchup",
     price: "0.20",
     price2:"",
